8e8b3ec68efbe971e9710d2baab0d28939ca7f15
sachin verma <43083013+sachinverma53121@users.noreply.github.com>
2019-11-06 01:32:45 +0530

    Create superuser.php

---
+++ superuser.php
@@ -0,0 +1,18 @@
+<?php 
+ include('config/db_connect.php');
+    
+
+ 
+?>
+
+<!DOCTYPE html>
+<html>
+  <?php include('templates/header.php'); ?>
+
+ <div class="container brown-text">
+ 
+ </div>	
+
+
+  <?php include('templates/footer.php'); ?>
+</html>
