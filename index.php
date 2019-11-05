5363491d8a4c8a571ddc1f7d9018fe309e6d8395
sachin verma <43083013+sachinverma53121@users.noreply.github.com>
2019-11-06 01:32:14 +0530

    First PHP + MySQL project

---
+++ index.php
@@ -0,0 +1,77 @@
+<?php 
+ 
+ 
+?>
+
+<!DOCTYPE html>
+<html>
+  <?php include('templates/header.php'); ?>
+
+  <!-- COOL GRID LAYOUT -->
+  	<!-- <div class="container">
+  		<div class="row first-row">
+			<div class="col s10 m9">Register Team</div>
+			<div class="col s2 m3"><i class="material-icons"></i></div> 
+		</div>
+		<div class="row first-row">
+			<div class="col s2 m3">content</div>
+			<div class="col s10 m9"><i class="material-icons"></i></div>
+		</div>
+		<div class="row first-row">
+			<div class="col s10 m9">content</div>
+			<div class="col s2 m3"><i class="material-icons"></i></div> 
+		</div>
+		<div class="row first-row">
+			<div class="col s2 m3">content</div>
+			<div class="col s10 m9"><i class="material-icons"></i></div>
+		</div>
+  	</div> -->
+
+   <div class="container" id="cardz">
+	<h2 class="brown-text">Getting Started...</h2>
+	<div class="row">
+	  <div class="col s12 l6">
+	  	<div class="card">
+	  	  <div class="card-image">	
+	  		 <img src="img/regteam.png" alt="image">
+	  		 <a href="registerteam.php" class="btn-floating blue pulse halfway-fab">
+	  		   <i class="material-icons">add</i>
+	  		 </a>
+	  		</div>
+	  	   <div class="card-content">
+	  		 <span class="card-title brown-text">Register Team</span>
+	  		 <blockquote>
+	  		  Superior Leaders Are Willing To Admit A Mistake And Cut Their Losses. Be Willing To Admit That You’ve Changed Your Mind. Don’t Persist When The Original Decision Turns Out To Be A Poor One.
+	  		 </blockquote> 
+	  	   </div>
+	  	   <div class="card-action">
+	  		 <a href="#terms"><span class="brown-text">more info...</span></a>
+	  	   </div>
+	  	</div>
+	  </div>
+	  <div class="col s12 l6">
+	  	<div class="card">
+	  	  <div class="card-image">
+	  		 <img src="img/regmatch.png" alt="image">
+	  		 <a href="fixamatch.php" class="btn-floating orange pulse halfway-fab">
+	  		   <i class="material-icons">edit</i>
+	  		 </a>
+	  		</div>
+	  	   <div class="card-content">
+	  		 <span class="card-title brown-text">Fix a Match</span>
+	  		 <blockquote>
+	  		  Superior Leaders Are Willing To Admit A Mistake And Cut Their Losses. Be Willing To Admit That You’ve Changed Your Mind. Don’t Persist When The Original Decision Turns Out To Be A Poor One.
+	  		 </blockquote>
+	  	   </div>
+	  	   <div class="card-action">
+	  		 <a href="#terms"><span class="brown-text">more info...</span></a>
+	  	   </div>
+	  	</div>
+	  </div>
+	</div>
+
+	
+
+
+  <?php include('templates/footer.php'); ?>
+</html>
