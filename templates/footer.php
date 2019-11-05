17a629a9ff6a20165f87a3b50eceb9b1d38b5f41
sachin verma <43083013+sachinverma53121@users.noreply.github.com>
2019-11-06 01:32:49 +0530

    Create footer.php

---
+++ templates/footer.php
@@ -0,0 +1,51 @@
+
+
+ <!-- <footer class="page-footer grey darken-2">
+    <div class="container">
+      <div class="row">
+        <div class="col s12 l6 right-align">
+          <h4>Reach To Us</h4>
+          <h5>Cricket Corp. Ltd.</h5>
+          <p>Old Bombay Road, Seema Bldg, Near Brahmin Vidyalaya, Charai, Thane (west)</p>
+          <p>Mumbai, Maharashtra - 400601</p>
+          <p>02225344921</p>  
+          <p>cricket-corp-ind@fakemail.net</p>
+        </div>
+        <div class="col s12 l4 offset-l1">
+          <h4>Connect To Us</h4>
+          <ul>
+            <li><a href="#" class="grey-text text-lighten-3">Facebook</a></li>
+            <li><a href="#" class="grey-text text-lighten-3">Twitter</a></li>
+            <li><a href="#" class="grey-text text-lighten-3">Linked In</a></li>
+            <li><a href="#" class="grey-text text-lighten-3">Instagram</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="footer-copyright grey darken-4">
+      <div class="container center-align">
+        &copy; 2019 Cricket Corporation India
+      </div>
+    </div>
+  </footer> -->
+ 
+
+
+  
+
+  <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+  <!-- Compiled and minified JavaScript -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+  <script type="text/javascript">
+  	$(document).ready(function() {
+  		$('.sidenav').sidenav();
+      $('.tooltipped').tooltip();
+      // $('.datepicker').datepicker({
+      // });
+  	})
+
+    function popUp(string) {
+      alert(string);
+    }
+  </script>
+</body>
