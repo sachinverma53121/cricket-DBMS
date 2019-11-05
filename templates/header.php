39503a3c713476fc1dd7062e45f02272592fed1f
sachin verma <43083013+sachinverma53121@users.noreply.github.com>
2019-11-06 01:32:53 +0530

    Create header.php

---
+++ templates/header.php
@@ -0,0 +1,49 @@
+<head>
+	<title>Cricket Match DBMS</title>
+	<!--Import Google Icon Font-->
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+	
+	<style>
+	  	nav .badge{
+    		position: relative;
+    		right: 25px;
+    	}
+    	blockquote{
+		  border-left: 5px solid #876969;
+		}
+
+
+	</style>
+</head>
+<body>
+	<nav class="nav-wrapper blue-grey darken-3 white-text">
+		<div class="container">
+		<a href="#" class="brand-logo">Cricket-Corporation</a>
+		<a href="#" class="sidenav-trigger" data-target="mobile-links">
+			<i class="material-icons">menu</i>
+		</a>
+		<ul class="right hide-on-med-and-down">
+		  <li><a href="index.php">Home</a></li>
+		  <li><a href="registerteam.php">Reg. Team</a></li>
+		  <li><a href="fixamatch.php">Fix Match</a></li>
+		  <li><a href="admin.php" data-tooltip="Admin Login" class="btn-floating blue-grey lighten-1 tooltipped z-depth-0">
+		  	<i class="material-icons">supervised_user_circle</i>
+		  </a></li>
+		  <li><span class="badge tooltipped white-text pink new" data-tooltip="5 new notifications available">5</span></li>
+		</ul> 
+		</div>
+	</nav>
+	<ul class="sidenav blue-grey darken-2" id="mobile-links">
+	  <li><a href="index.php" class="white-text">Home</a></li>
+		  <li><a href="registerteam.php" class="white-text">Reg. Team</a></li>
+		  <li><a href="fixamatch.php" class="white-text">Fix Match</a></li>
+	  <li><a href="admin.php" class="white-text">Admin Login</a></li>
+	</ul>
+
+
+
+
+
+
