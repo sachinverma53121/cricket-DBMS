79f77ee5937abfd5b66e2eb4a3863e30fc25eece
sachin verma <43083013+sachinverma53121@users.noreply.github.com>
2019-11-14 23:28:12 +0530

    Update superuser.php

--- superuser.php
+++ superuser.php
@@ -10,7 +10,41 @@
   <?php include('templates/header.php'); ?>
 
  <div class="container brown-text">
- 
+   <div class="center container"> <br>
+   	<ul class="collection with-header">
+   		<li class="collection-header"><h4>View Details...</h4></li>
+		 <li class="collection-item avatar box valign-wrapper">
+		    <i class="material-icons circle blue">event</i>
+		    <div class="title"><a href="regteams.php">Registered Teams</a></div>
+		    <div class="container">
+		      <p class="grey-text">See a list of registered team</p>
+		      <a class="secondary-content" href="regteams.php">
+		    	<i class="material-icons blue-text medium">keyboard_arrow_right</i>
+		      </a>
+		    </div>
+		 </li>
+		 <li class="collection-item avatar box valign-wrapper">
+		    <i class="material-icons circle blue">event_note</i>
+		    <span class="title"><a href="details.php">See Team Players</a></span>
+		    <div class="container">
+		      <p class="grey-text">See Details of a team</p>
+		      <a class="secondary-content" href="details.php">
+		    	<i class="material-icons blue-text medium">keyboard_arrow_right</i>
+		      </a>
+		    </div>
+		 </li> 
+		 <li class="collection-item avatar box valign-wrapper">
+		    <i class="material-icons circle blue">event_available</i>
+		    <span class="title"><a href="matches.php">Matche Schedules</a></span>
+		    <div class="container">
+		      <p class="grey-text">See Matches of a team</p>
+		      <a class="secondary-content" href="matches.php">
+		    	<i class="material-icons blue-text medium">keyboard_arrow_right</i>
+		      </a>
+		    </div>
+		 </li>  
+   	</ul>	
+   </div>
  </div>	
 
 
