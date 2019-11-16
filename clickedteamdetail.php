188fc947b9d6a69eb8ddf1ef0157d2a010e0b8b3
sachin verma <43083013+sachinverma53121@users.noreply.github.com>
2019-11-16 13:31:47 +0530

    Update clickedteamdetail.php

--- clickedteamdetail.php
+++ clickedteamdetail.php
@@ -42,18 +42,113 @@
   // // fetch result in array format
   // $managerphone = mysqli_fetch_assoc($result_managerphone); 
 
-  mysqli_free_result($result_team_name);
-  mysqli_free_result($result_country);
-  mysqli_free_result($result_coach);
-  mysqli_free_result($result_manager);
-  mysqli_close($conn);
-
   // print_r($team);
   // print_r($country);
   // print_r($coach);
   // print_r($manager);
 
  }   
+
+ if(isset($_POST['delete'])) {
+  $id_to_delete = mysqli_real_escape_string($conn, $_POST['id_to_delete']);
+ 
+ 
+  $sql = "SELECT match_no FROM teammatch WHERE team_name= '$id_to_delete'";
+  $result = mysqli_query($conn, $sql);
+
+  //fetch the resulting rows as array
+  $match_records = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+  foreach ($match_records as $match_record) {
+    $match_no_delete = $match_record['match_no'];
+    $sql_delete = "DELETE FROM teammatch WHERE match_no = $match_no_delete ";
+    $sql_delete_match = "DELETE FROM matchh WHERE match_no = $match_no_delete ";
+     if(mysqli_query($conn, $sql_delete) && mysqli_query($conn, $sql_delete_match)) {
+      //sucess
+    } else {
+      echo 'query error: '.mysqli_error($conn);
+    }
+
+  }
+  $delete_players = "DELETE FROM player WHERE team_name = '$id_to_delete'";
+  $delete_coach = "DELETE FROM coach WHERE team_name = '$id_to_delete'";
+     if(mysqli_query($conn, $delete_players) && mysqli_query($conn, $delete_coach)) {
+      //sucess
+    } else {
+      echo 'query error: '.mysqli_error($conn);
+    }
+
+  $sql = "SELECT match_no FROM matchh WHERE opposition= '$id_to_delete'";
+  $result = mysqli_query($conn, $sql);
+
+  //fetch the resulting rows as array
+  $match_records = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+  foreach ($match_records as $match_record) {
+    $match_no_delete = $match_record['match_no'];
+    $sql_delete = "DELETE FROM teammatch WHERE match_no = $match_no_delete ";
+    $sql_delete_match = "DELETE FROM matchh WHERE match_no = $match_no_delete ";
+     if(mysqli_query($conn, $sql_delete) && mysqli_query($conn, $sql_delete_match)) {
+      //sucess
+    } else {
+      echo 'query error: '.mysqli_error($conn);
+    }
+
+  }
+
+  $sql = "SELECT manager_name FROM manager WHERE team_name= '$id_to_delete'";
+  $result = mysqli_query($conn, $sql);
+
+  //fetch the resulting rows as array
+  $managers = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+  foreach ($managers as $manager) {
+    $manager_name_delete = $manager['manager_name'];
+    $sql_delete_phone = "DELETE FROM managerphone WHERE manager_name = '$manager_name_delete' ";
+    $sql_delete_manager = "DELETE FROM manager WHERE team_name = '$id_to_delete' ";
+     if(mysqli_query($conn, $sql_delete_phone) && mysqli_query($conn, $sql_delete_manager)) {
+      //sucess
+    } else {
+      echo 'query error: '.mysqli_error($conn);
+    }
+
+  }
+
+  
+  $sql = "SELECT country_code FROM team WHERE team_name= '$id_to_delete'";
+  $sql_delete_team = "DELETE FROM team WHERE team_name = '$id_to_delete'";
+  $result = mysqli_query($conn, $sql);
+
+  //fetch the resulting rows as array
+  $country = mysqli_fetch_all($result, MYSQLI_ASSOC);
+  $country_code = $country[0]['country_code'];
+
+  $sql_delete_country = "DELETE FROM country WHERE country_code = '$country_code'";
+  if(mysqli_query($conn, $sql_delete_team) && mysqli_query($conn, $sql_delete_country)) {
+      header('Location: regteams.php'); 
+    } else {
+      echo 'query error: '.mysqli_error($conn);
+    }
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+  // if(mysqli_query($conn, $sql)) {
+  //   header('Location: index.php'); 
+  // } else {
+  //   echo 'query error: '.mysqli_error($conn);
+  // }
+} 
+
 
  
 ?>
@@ -68,7 +163,7 @@
   </a>
   </div>
 
-  <div class="container">
+  <div class="container center">
     <div class="container brown-text text-lighten-1">
       <h5 class="center">TEAM NAME</h5>
       <h6 class="center grey-text"><?php echo htmlspecialchars($team['team_name']); ?></h6>
@@ -79,8 +174,14 @@
       <h5 class="center">COUNTRY</h5>
       <h6 class="center grey-text"><?php echo htmlspecialchars($country['country_name']); ?></h6> 
 
-    </div>  
+    </div> <br>
+
+     <form action="clickedteamdetail.php?team_name=<?php echo $team['team_name'];?>" method="POST">
+      <input type="hidden" name="id_to_delete" value="<?php echo $team['team_name']; ?>">
+      <input type="submit" name="delete" value="Remove Team" class="btn brown lighten-1 z-depth-0">
+     </form> 
   </div>
+ 
 
 
 
